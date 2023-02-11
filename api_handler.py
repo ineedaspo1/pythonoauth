fc5fee06b62f8ccd814e4ac0cd78f14d9b32b7a4
Sean Baker <cos30degrees@gmail.com>
2023-02-11 13:34:37 -0500

    sean

---
+++ api_handler.py
@@ -0,0 +1,169 @@
+#sean Baker
+# 2023-02-11
+
+import requests
+import json
+
+class BrightspaceAPI:
+    def __init__(self, access_token, api_url):
+        self.access_token = access_token
+        self.api_url = api_url
+        self.headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+             'Content-Type': 'application/json',
+             'Accept': 'application/json'
+            
+        }
+        self.org_structure = {}
+        self.org_tree = []
+        self.org_courses = []
+
+    def get_courses(self):
+        url = self.api_url + '/d2l/api/lp/1.0/courses/'
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_enrollments(self, course_id):
+        url = self.api_url + '/d2l/api/lp/1.0/enrollments/' + str(course_id)
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_course_details(self, course_id):
+        url = self.api_url + '/d2l/api/lp/1.0/courses/' + str(course_id)
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_user_details(self, user_id):
+        url = self.api_url + '/d2l/api/lp/1.0/users/' + str(user_id)
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def create_discussion_topic(self, course_id, title, message):
+        url = self.api_url + '/d2l/api/lp/1.0/discussions/' + str(course_id)
+        payload = {
+            "Title": title,
+            "Message": message
+        }
+        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
+        return response.json()
+    def get_org_structure(self):
+        response = requests.get(f'{self.api_url}/d2l/api/lp/1.10/orgstructure/', headers=self.headers)
+        return json.loads(response.text)
+
+
+    def get_org_units(self, org_unit_id=6606):
+        url = self.api_url + '/d2l/api/lp/1.20/orgstructure/{}'.format(org_unit_id) + '/children/'
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        org_units = response.json()
+        return org_units
+
+    def check_course_completion(self, org_unit_id):
+        url = self.api_url + '/d2l/api/lp/1.0/enrollments/orgUnits/' + str(org_unit_id) + '/users/'
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        enrollments = response.json()
+        for enrollment in enrollments['Items']:
+            if enrollment['IsCourseCompleted'] == True:
+                return True
+        return False
+
+    def get_org_name(self, org_unit_id):
+        url = self.api_url + '/d2l/api/lp/1.0/orgstructure/' + str(org_unit_id)
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        org_unit = response.json()
+        return org_unit['Name']
+
+    def find_org_with_students(self):
+        url = self.api_url + '/d2l/api/lp/1.0/orgstructure/'
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        org_units = response.json()
+        for org_unit in org_units['Identifier'] == 'CourseOffering':
+            print(org_unit) 
+            if org_unit['Identifier'] == 'CourseOffering':
+                url = self.api_url + '/d2l/api/lp/1.0/enrollments/orgUnits/' + str(org_unit['Identifier']) + '/users/'
+                response = requests.get(url, headers=headers)
+                enrollments = response.json()
+                for enrollment in enrollments['Items']:
+                    if enrollment['User']['Identifier'] == self.get_user_id() and enrollment['User']['Profile']['Email'].endswith('@mycollege.edu'):
+                        return self.get_org_name(org_unit['ParentOrganizationUnitId'])
+        return None
+
+    def get_user_id(self):
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Accept': 'application/json'
+        }
+        url = self.base_url + '/d2l/api/lp/1.0/users/whoami'
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['Identifier']
+        else:
+            return None
+
+    
+
+    def get_org_tree(self, org_units=6606):
+        orgs_data = self.get_org_units(org_units)
+       
+        if len(orgs_data) != 0:
+            for org in orgs_data:
+                if org['Identifier'] not in self.org_structure:
+                    self.org_structure[org['Identifier']] = {
+                        "Name": org["Name"],
+                        "Type": {"code": org["Type"]["Code"], "name": org["Type"]["Name"]},
+                        "parentOrgUnitId": org["parentOrgUnitId"],
+                        "children": [],
+                        "courses": []
+                    }
+                if org['parentOrgUnitId'] != None:
+                    parent = self.org_structure.get(org["parentOrgUnitId"], {})
+                    parent.setdefault("children", []).append(org['Identifier'])
+
+                if "Course" not in org["Type"]["Code"]:
+                    self.org_tree.append(org["Identifier"])
+                else:
+                    courses = self.get_courses(org["Identifier"])
+                    self.org_structure[org['Identifier']]['courses'].extend(courses)
+                   
+        for org_id in self.org_tree:
+            self.get_org_tree(org_id, self.org_structure, self.org_courses)
+
+        return self.org_structure
+
+
+        
+
+    def get_courses(self, org_id=6606):
+
+        courses_response = requests.get(f'{self.api_url}/d2l/api/lp/1.0/courses/?orgUnitId={org_id}', headers=self.headers)
+        courses_data = courses_response.json()
+        for course in courses_data['Items']:
+            course_id = course['Identifier']
+            course_name = course['Name']
+            self.org_structure[org_id]['courses'].append({'id': course_id, 'name': course_name})
+
+        return self.org_structure[org_id]
+
+    def get_user_id_from_username(self, username):
+        url = self.api_url + '/d2l/api/lp/1.0/users/'
+        params = {'userName': username}
+        response = requests.get(url, headers=self.headers, params=params)
+        if response.status_code == 200:
+            users = response.json()
+        return users['UserId']
--- getAuthorization.py
+++ getAuthorization.py
@@ -1,8 +1,10 @@
 import requests_oauthlib
-from requests_oauthlib import OAuth2Session
 import webbrowser
 import json
-import re
+import os
+import easygui
+import requests
+from api_handler import BrightspaceAPI
 
 from appFuncs import getConfig as getConfig
 from appFuncs import saveConfig as saveConfig
@@ -13,31 +15,110 @@ from appFuncs import accessToken as accessToken
 tokenConfigFile = 'tokenConfig'
 
 conf = getConfig(tokenConfigFile)
-auth = authorize(tokenConfigFile)
 
-webbrowser.open_new_tab(auth)
+if 'access_token' in conf and 'refresh_token' in conf:
+    print('Access token and refresh token already available.')
+else:
+    auth = authorize(tokenConfigFile)
+    webbrowser.open_new_tab(auth)
+    print('Authorizing...')
+    print('Please enter the URL from the browser:')
+    enterAuthCode = input()
 
-try:
-    while True:
-        with open("temp.html", "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                match = re.search(r"code=([\w-]+)&state=([\w-]+)", line)
-                if match:
-                    conf['auth_code'] = match.group(1)
-                    conf['state'] = match.group(2)
-                    break
-        if conf.get('auth_code'):
-            break
-except FileNotFoundError:
-    pass
+    ac = parseAuthCode(enterAuthCode)
+    conf['auth_code'] = ac[0]
+    conf['state'] = ac[1]
 
-accToken = accessToken(conf)
-print('\n\n' + str(accToken) + '\n\n')
-print(conf['auth_code'])
+    accToken = accessToken(conf)
+    conf['access_token'] = accToken['access_token']
+    conf['refresh_token'] = accToken['refresh_token']
+    conf['expires_at'] = accToken['expires_at']
+    saveConfig(tokenConfigFile, conf)
+    print('Access token and refresh token saved.')
 
-conf['access_token'] = accToken['access_token']
-conf['refresh_token'] = accToken['refresh_token']
-conf['expires_at'] = accToken['expires_at']
-token = {'access_token': conf['access_token'], 'token_type': 'Bearer' }
-saveConfig(tokenConfigFile, conf)
+print('Access token:', conf['access_token'])
+print('Refresh token:', conf['refresh_token'])
+include_whoami = easygui.ynbox(
+    'Would you like to include a WHOAMI GET call in the application?')
+if include_whoami:
+    headers = {'Authorization': 'Bearer ' + conf['access_token']}
+    r = requests.get(
+        'https://fieldx.brightspace.com/d2l/api/whoami', headers=headers)
+    if r.status_code == 200:
+        print(r.json())
+    else:
+        print('WHOAMI GET call failed with status code:', r.status_code)
+
+
+access_token = conf["access_token"]
+api_url = "https://davidm.brightspacedemo.com"
+
+api = BrightspaceAPI(access_token, api_url)
+
+print("What would you like to do?")
+print("1. Get Courses")
+print("2. Get Enrollments")
+print("3. Get Course Details")
+print("4. Get User Details")
+print("5. Create Discussion Topic")
+print("6. Get Org Units")
+print("7. Check Course Completion")
+print("8. Get Org Name")
+print("9. Find Org with Students")
+print("10. Get User ID")
+print("11. Get Org Structure")
+print("12. Get User ID from Username")
+
+choice = 1
+
+while choice != 0:
+
+    choice = int(input("Enter the number of your choice: ")) 
+    if choice == 1:
+        courses = api.get_courses()
+        print(courses)
+    elif choice == 2:
+        course_id = int(input("Enter the course ID: "))
+        enrollments = api.get_enrollments(course_id)
+        print(enrollments)
+    elif choice == 3:
+        course_id = int(input("Enter the course ID: "))
+        course_details = api.get_course_details(course_id)
+        print(course_details)
+    elif choice == 4:
+        user_id = int(input("Enter the user ID: "))
+        user_details = api.get_user_details(user_id)
+        print(user_details)
+    elif choice == 5:
+        course_id = int(input("Enter the course ID: "))
+        title = input("Enter the title: ")
+        message = input("Enter the message: ")
+        response = api.create_discussion_topic(course_id, title, message)
+        print(response)
+    elif choice == 6:
+        org_units = api.get_org_units()
+        print(org_units)
+    elif choice == 7:
+        org_unit_id = int(input("Enter the org unit ID: "))
+        completion = api.check_course_completion(org_unit_id)
+        print(completion)
+    elif choice == 8:
+        org_unit_id = int(input("Enter the org unit ID: "))
+        org_name = api.get_org_name(org_unit_id)
+        print(org_name)
+    elif choice == 9:
+        org_with_students = api.find_org_with_students()
+        print(org_with_students)
+    elif choice == 10:
+        user_id = api.get_user_id()
+        print(user_id)
+    elif choice == 11:
+        org_structure = api.get_org_structure()
+        print(org_structure)
+
+    elif choice == 12:
+        username = input("Enter the username: ")
+        user_id = api.get_user_id_from_username(username)
+        print(user_id)
+
+        
