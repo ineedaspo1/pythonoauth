5c1292a9ab18e3b039d1fcea1d942da0b606a017
Sean Baker <cos30degrees@gmail.com>
2023-02-11 01:10:20 -0500

    added api ahandler

--- getAuthorization.py
+++ getAuthorization.py
@@ -1,19 +1,9 @@
-# Damon Adkins - kk4tza@gmail.com
-# 11/27/2019
-#
-# This application will obtain refresh token
-# for D2l/Brightspace oAuth 2.0 api applications.
-#
-# Python 3.6
-#
-# pip install requests, requests_oauthlib and easygui
-#
-
 import requests_oauthlib
 from requests_oauthlib import OAuth2Session
 import webbrowser
 import json
-import easygui
+import re
+
 from appFuncs import getConfig as getConfig
 from appFuncs import saveConfig as saveConfig
 from appFuncs import authorize as authorize
@@ -26,11 +16,21 @@ conf = getConfig(tokenConfigFile)
 auth = authorize(tokenConfigFile)
 
 webbrowser.open_new_tab(auth)
-enterAuthCode = easygui.enterbox('Enter URL from Browser:')
 
-ac = parseAuthCode(enterAuthCode)
-conf['auth_code'] = ac[0]
-conf['state'] = ac[1]
+try:
+    while True:
+        with open("temp.html", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                match = re.search(r"code=([\w-]+)&state=([\w-]+)", line)
+                if match:
+                    conf['auth_code'] = match.group(1)
+                    conf['state'] = match.group(2)
+                    break
+        if conf.get('auth_code'):
+            break
+except FileNotFoundError:
+    pass
 
 accToken = accessToken(conf)
 print('\n\n' + str(accToken) + '\n\n')
@@ -41,7 +41,3 @@ conf['refresh_token'] = accToken['refresh_token']
 conf['expires_at'] = accToken['expires_at']
 token = {'access_token': conf['access_token'], 'token_type': 'Bearer' }
 saveConfig(tokenConfigFile, conf)
-
-
-
-
